f8bfc8c73be9b4fade1b4c9e616e6e60f0935833
Jayden Cowley <jcowley16@gmail.com>
2022-08-20 13:39:35 -0600

    setting up database

---
+++ db/schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE thought (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    userid INTEGER NOT NULL,
+    date_ VARCHAR(30) NOT NULL, 
+    createdAt datetime,
+    updatedAt datetime
+
+);
---
+++ db/seeds.sql
@@ -0,0 +1,21 @@
+INSERT INTO users (first_name, last_name, email, password)
+VALUES
+    ('Ronald', 'Firbank', 'RonFirbank@gmail.com', 'pass1234567'),
+    ('Virginia', 'Woolf', 'VirginiaW@gmail.com', 'pass1234567'),
+    ('Piers', 'Gaveston', 'PiersGave@gmail.com', 'pass1234567'),
+    ('Charles', 'LeRoi', 'CleRoi@gmail.com', 'pass1234567'),
+    ('Katherine', 'Mansfield', 'KathyMan@gmail.com', 'pass1234567'),
+    ('Dora', 'Carrington', 'DoraCarring@gmail.com', 'pass1234567'),
+    ('Edward', 'Bellamy', 'EdwardBell@gmail.com', 'pass1234567'),
+    ('Montague', 'Summers', 'Montaguesss@gmail.com', 'pass1234567'),
+    ('Octavia', 'Butler', 'DocOctavia@gmail.com', 'pass1234567'),
+    ('Unica', 'Zurn', 'UniqueZurn@gmail.com', 'pass1234567');
+
+INSERT INTO thought (body, date_, userid)
+VALUES
+    ('Lifting', '2022-12-21', 1),
+    ('Running', '2022-01-25', 2),
+    ('Swimming', '2022-12-31', 1),
+    ('Walking', '2022-11-24', 2),
+    ('Crossfit', '2022-10-14', 1),
+    ('Jog', '2022-08-02', 2)
