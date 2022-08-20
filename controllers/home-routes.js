a870119e990048a27567a7e4d73984527a8a3182
Jayden Cowley <jcowley16@gmail.com>
2022-08-20 13:33:00 -0600

    adding home-routes

---
+++ controllers/home-routes.js
@@ -0,0 +1,33 @@
+const router = require('express').Router();
+
+const { User } = require('../models');
+//const addWorkoutRoutes = require('./workout-routes'); 
+
+router.get('/', (req, res) => {
+  res.render('dashboard')
+});
+router.get('/createUser', (req, res) => {
+  res.render('createUser')
+})
+// Login route
+router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect to the homepage
+  if (req.session.loggedIn) {
+    res.redirect('/dashboard');
+    return;
+  }
+  // Otherwise, render the 'login' template
+  res.render('login');
+});
+router.get('/dashboard', (req, res) => {
+  
+  res.render('dashboard', 
+  //{loggedIn: req.session.loggedIn}
+  );
+})
+
+
+
+
+
+module.exports = router;
